f22b4ffa20d0ad90c3c68267947f6b455a65f3d7
Serhii_Y <dj.ser961@gmail.com>
2019-10-14 23:34:08 +0300

    Updates

---
+++ precache-manifest.ead90ecf2d603698fd0b5ba693f3e70a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d39c4140a0bb65b2d6d2ef4f9a9eb1e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "be29cc037a3093dbe91f",
+    "url": "/goit-react-hw-01-components/static/css/main.f341d06c.chunk.css"
+  },
+  {
+    "revision": "bf0c57c7dadf99ff1ed4",
+    "url": "/goit-react-hw-01-components/static/js/2.f2b7e59f.chunk.js"
+  },
+  {
+    "revision": "be29cc037a3093dbe91f",
+    "url": "/goit-react-hw-01-components/static/js/main.a5e974cc.chunk.js"
+  },
+  {
+    "revision": "bfb5c25008325e80740b",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.040dc6c8.js"
+  }
+]);
